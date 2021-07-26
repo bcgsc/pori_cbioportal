3ede66d8b722f6f8e6b4720d874f37733cd6e2e1
Caralyn Reisle <creisle@bcgsc.ca>
2021-07-26 13:16:00 -0700

    Use folder as input

--- pori_cbioportal/main.py
+++ pori_cbioportal/main.py
@@ -8,44 +8,51 @@ from .util import LOG_LEVELS
 
 def command_interface():
     parser = argparse.ArgumentParser()
-    parser.add_argument('study_id', help='The study ID (ex. dlbc_tcga_pan_can_atlas_2018)')
+    parser.add_argument('--study_id', help='The study ID (ex. "BRCA TCGA PANCAN (2018)")')
     parser.add_argument(
-        'patient_data',
+        'data_folder', help='path to the folder containing the cbioportal export files'
+    )
+    parser.add_argument(
+        '--strict',
+        action='store_true',
+        default=False,
+        help='Flag to indicate the program should stop if any report fails to upload',
+    )
+    parser.add_argument(
+        '--patient_data',
+        default='data_clinical_patient.txt',
         help='The clinical patient data for this study (ex. data_clinical_patient.txt)',
     )
     parser.add_argument(
-        'sample_data',
+        '--sample_data',
+        default='data_clinical_sample.txt',
         help='The clinical sample data for this study (ex. data_clinical_sample.txt)',
     )
     parser.add_argument(
         '--expression',
-        '-e',
+        default='data_RNA_Seq_v2_mRNA_median_all_sample_Zscores.txt',
         help='The mRNA RNA Seq zscore data. (ex. data_RNA_Seq_v2_mRNA_median_all_sample_Zscores.txt)',
-        required=True,
     )
     parser.add_argument(
         '--small_mutations',
-        '-m',
+        default='data_mutations_extended.txt',
         help='The small mutation data (ex. data_mutations_extended.txt)',
-        required=True,
     )
     parser.add_argument(
         '--discrete_cna',
-        '-d',
+        default='data_CNA.txt',
         help='The discrete copy variant data (ex. data_CNA.txt)',
-        required=True,
     )
     parser.add_argument(
         '--continuous_cna',
-        '-c',
         help='The continuous (log2cna) copy variant data (ex. data_log2CNA.txt)',
-        required=True,
+        default='data_log2CNA.txt',
     )
     parser.add_argument(
         '--ipr_project', help='The IPR project to upload this report to', default='TEST'
     )
     parser.add_argument(
-        '--fusions', '-f', help='The fusion data (ex. data_fusions.txt)', required=True,
+        '--fusions', '-f', help='The fusion data (ex. data_fusions.txt)', default='data_fusions.txt'
     )
     parser.add_argument(
         '--username',
@@ -69,6 +76,20 @@ def command_interface():
     )
     args = parser.parse_args()
 
+    def resolve_data_path(filepath, required=True):
+        """
+        Join the path to the data folder if it is relative, don't if it is absolute
+        """
+
+        if filepath.startswith('/') and os.path.exists(os.path.join('/', filepath)):
+            return filepath
+        new_path = os.path.join(args.data_folder, filepath)
+        if not os.path.exists(new_path):
+            if required:
+                raise argparse.ArgumentTypeError(f'file does not exist: {new_path} ({filepath})')
+            return None
+        return new_path
+
     logging.basicConfig(
         level=LOG_LEVELS['info'],
         format='%(asctime)s %(name)s %(levelname)s %(message)s',
@@ -76,18 +97,20 @@ def command_interface():
     )
 
     generate_reports(
-        args.study_id,
-        args.patient_data,
-        args.sample_data,
-        continuous_copy_variants_filename=args.continuous_cna,
-        discrete_copy_variants_filename=args.discrete_cna,
-        small_mutations_filename=args.small_mutations,
-        expression_filename=args.expression,
-        fusions_filename=args.fusions,
+        args.study_id if args.study_id else os.path.basename(args.data_dir),
+        resolve_data_path(args.patient_data),
+        resolve_data_path(args.sample_data),
+        continuous_copy_variants_filename=resolve_data_path(args.continuous_cna, False),
+        discrete_copy_variants_filename=resolve_data_path(args.discrete_cna),
+        small_mutations_filename=resolve_data_path(args.small_mutations),
+        expression_filename=resolve_data_path(args.expression, False),
+        fusions_filename=resolve_data_path(args.fusions, False),
         username=args.username,
         password=args.password,
         ipr_project=args.ipr_project,
         ipr_url=args.ipr_url,
         graphkb_url=args.graphkb_url,
         patients_subset=args.patients_subset,
+        strict=args.strict,
+        debugging_filename=args.debugging_filename
     )
